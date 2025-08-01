fffca8ee4bf9a81d478e392b286023978d23dc91
Piotr Stojanow <piotr.stojanow@gmail.com>
2025-08-01 14:11:10 +0200

    chore: release v0.2.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // @match        *://*.youtube.com/*
 // @run-at      document-start
 // @grant       none
-// @version      0.2.0
+// @version      0.2.1
 // @author      Piotr Stojanow (https://github.com/psto/)
 // @license     MIT
 // @description Redirect a YouTube channel home page straight to the videos tab.
